14d084781ca714dbe7d143d4500feeee2424a1df
A <you@example.com>
2016-03-30 18:41:28 +0200

    init

---
+++ common/mixins/company.js
@@ -0,0 +1,20 @@
+module.exports = function (Model, options) {  
+  Model.afterRemote('find', function(ctx, user, next){
+
+    if (ctx.accessToken)
+      console.log(ctx.accessToken.userId);
+    var res = [];
+    if(ctx.result) {
+      if(Array.isArray(ctx.result)) {
+        ctx.result.forEach(function (result) {
+          if (result.companyid === 1)
+            res.push(result);
+        });
+      } else {
+
+      }
+    }
+    ctx.result = res;
+    next();
+  });
+}
